eb85bd4429e3452560d07ed51166721f59e96fda
ylacaze <72870976+ylacaze@users.noreply.github.com>
2025-01-22 14:30:40 +0100

    fonction PosToXY

--- main.py
+++ main.py
@@ -2,8 +2,24 @@ from extract_frames import VideoProcessor
 from image_edge_processor import ImageEdgeProcessor
 from model import create_unet_model, preprocess_images
 from tensorflow.keras.optimizers import Adam
+from PIL import Image
+
 import os
 os.environ["OPENCV_FFMPEG_READ_ATTEMPTS"] = "10000"
+
+def PosToXY(lat,lon):
+
+    # Ouvrir l'image
+    image = Image.open('data/Annecy.png')
+
+    # Obtenir les dimensions de l'image
+    width, height = image.size
+
+    xratio = (lon - 0.211025) / (0.2136273 - 0.211025)
+    yratio = 1.0 - ((lat - 47.9411527) / (47.943917 - 47.9411527))
+    x = int(float(width - 512)*xratio+0.5) + (512 / 2) + 64
+    y = int(float(height - 512)*yratio+0.5) + (512 / 2) + 64
+    return (x,y)
 
 if __name__ == "__main__":
     # VideoProcessor.extract_frames("./data/20240914_target.mp4", "./output/")
